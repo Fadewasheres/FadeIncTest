f8f7b2ab74956d5f6aac8db3398f99aa89b586a9
FadeScripting <79726377+Fadewasheres@users.noreply.github.com>
2024-07-25 16:14:26 +0200

    Create blacklisted.lua

---
+++ blacklisted.lua
@@ -0,0 +1,25 @@
+--[[
+
+Blacklisted users
+
+!! YOU ARE A CERTIFIED SKID IF YOU THINK YOU ARE LEAKING THIS BY FINDING IT
+
+]]
+
+local BL_TBL = {
+ [1576756053] = 'Chariotsware meat rider, problematic',
+ [2205701910] = 'KILL YOURSELF',
+ [3731002752] = 'Sending CP in my server with an alt. PS.. You did a terrible job at covering your tracks',
+ [2704543218] = 'Crack attempt (x13)',
+ [2671971680] = 'Retarded',
+ [2954177801] = 'Kill yourself',
+ [2710395518] = 'Avertising servers like a retard | DM sillehfade to be unblacklisted.',
+ [3875571556] = 'You didnt actually have premium lmfao | DM sillehfade to be unblacklisted.',
+ [3871654427] = 'lmao',
+ [3594574567] = 'Stop DMing ex mods asking them to date your "rich" friend XD',
+ [3984781051] = 'You arent an Oblivion admin XD who r u',
+ [12203646] = 'PGED tix account | imagine paying 1$ for that shit LMAOOO',
+ [6097362430] = 'no words dont send cp etc. | DM sillehfade to be unblacklisted.'
+}
+
+return BL_TBL
